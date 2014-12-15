50e22c4c32497773f18d29ed0ade3770c653df04
lovdanny <dawnmy@163.com>
2014-12-15 15:14:33 +0100

    Create one_line_commands.sh
    A collection of useful one line commands

---
+++ one_line_commands.sh
@@ -0,0 +1,4 @@
+# clip the reads to a particular length
+paste - - - - < in.fastq | gawk -F"\t" '{print $1"\t"substr($2,1,50)"\t"$3"\t"substr($4,1,50)}'|tr "\t" "\n" > out_len50.fastq
+# extarct specific reads in terms of the identifier list file
+paste - - - - < in.fastq|fgrep --file read_identifier_list.txt|tr "\t" "\n" > out.fastq
