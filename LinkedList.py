8160e2b2d3fe1cb8cdf05c3df12e55c641cf9803
Z.-L. Deng <dawnmy@163.com>
2018-03-19 19:58:26 +0100

    add insert function

--- LinkedList.py
+++ LinkedList.py
@@ -10,16 +10,6 @@ class LinkedList(object):
         self.tail = None
         self.size = 0
 
-    def add(self, val):
-        new_node = ListNode(val)
-        if self.head is None:
-            self.head = new_node
-        else:
-            self.tail.next = new_node
-
-        self.tail = new_node
-        self.size += 1
-
     def __str__(self):
         current = self.head
         values = []
@@ -29,6 +19,16 @@ class LinkedList(object):
             current = current.next
 
         return " -> ".join(values)
+
+    def add(self, val):
+        new_node = ListNode(val)
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+
+        self.tail = new_node
+        self.size += 1
 
     def rm_value(self, v):
         previous = None
@@ -64,8 +64,29 @@ class LinkedList(object):
             current = current.next
             current_idx += 1
 
+    def insert(self, idx, v):
+        current = self.head
+        previous = None
+        current_idx = 0
+        node = ListNode(v)
+
+        while current is not None:
+            if current_idx == idx:
+                if previous is None:
+                    self.head = node
+                    # current.next
+                else:
+                    previous.next = node
+
+                node.next = current
+                self.size += 1
+
+            previous = current
+            current = current.next
+            current_idx += 1
+
+
 if __name__ == "__main__":
     LL = LinkedList()
-
     for i in range(0, 10):
         LL.add(i)
